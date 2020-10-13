9c0f0f6890ec99da24ca4a0f33d5acda76d25873
Michael Orzel <56739010+michael-orzel@users.noreply.github.com>
2020-10-13 16:11:50 -0400

    Create site.php

---
+++ site.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+        <?php
+            $party = array("Mario", "Mallow", "Geno", "Bowser");
+
+            $party[4] = "Peach";
+            echo count($party);
+        ?>
+    </body>
+</html>
